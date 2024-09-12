e258df4c6137d14a0a9475afc7c2771797f08a0f
lopezrnl <ranielcarllopez13@gmail.com>
2024-09-12 17:33:33 +0800

    Add files via upload

---
+++ Activity.java
@@ -0,0 +1,5 @@
+public class Activity{
+    public static void main(String[] args){
+        
+    }
+}
---
+++ Dog.java
@@ -0,0 +1,22 @@
+public class Dog {
+    String name;
+    boolean isMale;
+    int numberOfPuppies;
+    Dog[] Puppies = new Dog[3];
+    Dog name;
+
+    public String[] showName(){
+        String[] names = new String[3];
+        return name;
+    }
+
+    public int setNumOfPuppies(){
+        numberOfPuppies = 3;
+        return numberOfPuppies;
+    }
+
+    public void showNumOfPuppies(){
+        System.out.println(numberOfPuppies);
+    }
+}
+
