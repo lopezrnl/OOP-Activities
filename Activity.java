d6ee02b56512e0e842e0cdd5bbf3827a54c99ee2
lopezrnl <ranielcarllopez13@gmail.com>
2024-09-20 07:56:24 +0800

    Update Activity.java

--- Activity.java
+++ Activity.java
@@ -2,20 +2,20 @@ public class Activity {
     public static void main(String[] args) { 
         Dog awaw = new Dog();
         awaw.name = "Dido"; 
-        awaw.puppies = 3; 
+        awaw.puppies = 4; 
         awaw.isMale = true; 
 
         awaw.doggo[0] = "Batangas City";
         awaw.doggo[1] = "Hanabishi";
-        awaw.doggo[2] = "ID Lace"
-        awaw.doggo[3] = "Coby"
+        awaw.doggo[2] = "ID Lace";
+        awaw.doggo[3] = "Coby";
 
         System.out.println(awaw.dogname()); 
         System.out.println("woof! woof!"); 
         System.out.println(awaw.numofpuppies()); 
 
-        for (int i = 0; i < 3; i++) { 
-            System.out.println(awaw.doggy[i]); 
+        for (int i = 0; i < 4; i++) { 
+            System.out.println(awaw.doggo[i]); 
         }
         System.out.println("All information about the dog has been displayed successfully."); 
     }
