fde38af01c5b81dc2675e43e303b378be69e8c46
lopezrnl <ranielcarllopez13@gmail.com>
2024-12-23 15:18:52 +0800

    Student.java

---
+++ Student.java
@@ -0,0 +1,25 @@
+public class Student {
+    // Private attributes
+    private int Student_Id;
+    private String Name;
+
+    // Setter for Student_Id
+    public void setStudentId(int Student_Id) {
+        this.Student_Id = Student_Id;
+    }
+
+    // Getter for Student_Id
+    public int getStudentId() {
+        return Student_Id;
+    }
+
+    // Setter for Name
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    // Getter for Name
+    public String getName() {
+        return Name;
+    }
+}
